d23ee6a1d7dc38ef0dbb354708aa41e7d6719b7b
gikat <gikata.38@yandex.ru>
2026-02-25 18:55:24 +0300

    build(db): recreate database with postgis support

--- 2026_02_20_1458-ed05973640b0_generate_test_data.py
+++ 2026_02_20_1458-ed05973640b0_generate_test_data.py
@@ -1,22 +1,3 @@
-"""Расширенные тестовые данные: 15 зданий, 25 организаций, ~35 видов деятельности до 3 уровня
-
-Revision ID: ed05973640b0
-Revises: 1fe7879d23ef
-Create Date: 2026-02-20 14:58:23.226608
-"""
-
-from typing import Sequence, Union
-from alembic import op
-import sqlalchemy as sa
-import logging
-
-revision: str = "ed05973640b0"
-down_revision: Union[str, Sequence[str], None] = "1fe7879d23ef"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
-
-logger = logging.getLogger("alembic.runtime.migration")
-
 
 def upgrade() -> None:
     logger.info(f"=== APPLYING {revision} ===  расширенные тестовые данные")
